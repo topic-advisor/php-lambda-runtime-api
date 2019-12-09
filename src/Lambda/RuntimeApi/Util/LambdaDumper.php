63d411dedf25610133e803cc9cd6dd8cdc8a478b
Andrew Matthews <andrew@andymdesign.com.au>
2019-12-09 16:15:44 +1000

    Add newline to end of dumps

--- src/Lambda/RuntimeApi/Util/LambdaDumper.php
+++ src/Lambda/RuntimeApi/Util/LambdaDumper.php
@@ -6,6 +6,6 @@ class LambdaDumper
 {
     public static function dump($data)
     {
-        file_put_contents('php://stdout', json_encode($data));
+        file_put_contents('php://stdout', json_encode($data) ."\n");
     }
 }
