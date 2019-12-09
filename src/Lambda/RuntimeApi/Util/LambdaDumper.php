1a27fa8356a45d568e6e6abca73956831a5422e6
Andrew Matthews <andrew@andymdesign.com.au>
2019-12-09 15:32:15 +1000

    Make changes required for API Gateway

---
+++ src/Lambda/RuntimeApi/Util/LambdaDumper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TopicAdvisor\Lambda\RuntimeApi\Util;
+
+class LambdaDumper
+{
+    public static function dump($data)
+    {
+        file_put_contents('php://stdout', json_encode($data));
+    }
+}
